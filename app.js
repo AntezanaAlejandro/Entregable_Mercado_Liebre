3e005f5f3c7843f563089aac76806dd303f230a8
AntezanaAlejandro <antezana1299@gmail.com>
2022-06-20 21:39:45 -0300

    add ruteo heroku

--- app.js
+++ app.js
@@ -14,5 +14,9 @@ app.get('/creacionDeCuenta', (req, res)=>{
 app.get('/ingresaATuCuenta', (req, res)=>{
   res.sendFile(path.resolve(__dirname,'./views/login.html'));
 })
-//Ruteo de servidor
+//Ruteo LOCAL
 app.listen(3001, ()=> console.log("Servidor escuchando en puerto 3001"));
+//ruteo HEROKU
+app.set('puerto',process.env.PORT || 3000);
+app.listen(app.get('puerto'), ()=>console.log(`Servidor escuchando en puerto ${app.get('puerto')}`));
+
